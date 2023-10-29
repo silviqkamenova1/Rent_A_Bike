25a95a5c9b20732210bb3bba7b793070e6c1a054
silviqkamenova1 <117118888+silviqkamenova1@users.noreply.github.com>
2023-10-29 21:00:35 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
-import './App.css';
-import Home from './components/Home';
+import React from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Banner from './components/Banner/Banner';
+import OurCycles from './components/OurCycles/OurCycles';
+import AboutStore from './components/AboutStore/AboutStore';
+import Customers from './components/Customer/Customers';
+import News from './components/News/News';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
+import Copyright from './components/Copyright/Copyright';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  );
+export default function App() {
+   // function openNav() {
+   //    document.getElementById("mySidenav").style.width = "250px";
+   //    document.getElementById("main").style.marginLeft = "250px";
+   //  }
+
+   //  function closeNav() {
+   //    document.getElementById("mySidenav").style.width = "0";
+   //    document.getElementById("main").style.marginLeft= "0";
+
+   //  }
+
+   //  $("#main").click(function(){
+   //      $("#navbarSupportedContent").toggleClass("nav-normal")
+   //  })
+   return (
+      <>
+         <body>
+            <div className="header_section header_bg">
+               <Navigation />
+               <Banner />
+            </div>
+
+            <OurCycles />
+            <AboutStore />
+            <Customers />
+            <News />
+            <Contact />
+
+            <Footer />
+
+            <Copyright />
+
+         </body>
+      </>
+   );
 }
 
-export default App;
