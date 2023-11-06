26f8de6b765e72c778f175effa78c3ce204f9a28
silviqkamenova1 <117118888+silviqkamenova1@users.noreply.github.com>
2023-11-06 21:10:15 +0200

    add

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,45 +2,49 @@ import React from "react";
 
 export default function Navigation() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a href="index.html" className="logo"><img alt='' src="assets/images/logo.png" /></a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse " >
-                <ul className="navbar-nav mr-auto ">
-                    <li className="nav-item active ">
-                        <a className="nav-link" href="/">Home</a>
-                    </li>
-                    {/* <li className="nav-item">
+        
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a href="index.html" className="logo"><img alt='' src="assets/images/logo.png" /></a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                
+                    <div className="collapse navbar-collapse" >
+                        <ul className="navbar-nav mr-auto ">
+                            <li className="nav-item active ">
+                                <a className="nav-link" href="/">Home</a>
+                            </li>
+                            {/* <li className="nav-item">
                         <a className="nav-link" href="/about">About</a>
                     </li> */}
-                    <li className="nav-item">
-                        <a className="nav-link" href="/create">Add Bike</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/catalog">Shop</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/news">News</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/contact">Contact Us</a>
-                    </li>
-                </ul>
-                <form className="form-inline my-2 my-lg-0">
-                    <div className="login_menu">
-                        <ul>
-                            <li><a href="/login">Login</a></li>
-                            <li><a href="/register">Register</a></li>
-                            <li><a href="/logout">Logout</a></li>
-                            <li><a href="/cycle/trolly"><img alt='' src="assets/images/trolly-icon.png" /></a></li>
-                            <li><a href="/cycle/search"><img alt='' src="assets/images/search-icon.png" /></a></li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/create">Add Bike</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/catalog">Shop</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/news">News</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/contact">Contact Us</a>
+                            </li>
                         </ul>
+                        <form className="form-inline my-2 my-lg-0">
+                            <div className="login_menu">
+                                <ul>
+                                    <li><a className="nav-btns" href="/login">Login</a></li>
+                                    <li><a className="nav-btns" href="/register">Register</a></li>
+                                    <li><a className="nav-btns" href="/logout">Logout</a></li>
+                                    <li><a href="/cycle/trolly"><img alt='' src="assets/images/trolly-icon.png" /></a></li>
+                                    <li><a href="/cycle/search"><img alt='' src="assets/images/search-icon.png" /></a></li>
+                                </ul>
+                            </div>
+                            <div></div>
+                        </form>
                     </div>
-                    <div></div>
-                </form>
-            </div>
-        </nav>
+                
+            </nav>
+      
     );
 }
