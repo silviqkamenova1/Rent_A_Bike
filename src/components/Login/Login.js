f0ab0635213ed082d2abd2640a8f5f0dd9fc1b5c
silviqkamenova1 <117118888+silviqkamenova1@users.noreply.github.com>
2023-11-09 20:14:41 +0200

    add

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,23 +1,26 @@
 import React from "react";
 
 export default function Login() {
-    return(
-        <div className="contact_section layout_padding">
-        <div className="container">
-           <div className="contact_main">
-              <h1 className="request_text">Login</h1>
-              <form action="/action_page.php">
-                 <div className="form-group">
-                    <input type="text" className="email-bt" placeholder="Email" name="email"/>
-                 </div>
-                 <div className="form-group">
-                    <input type="text" className="email-bt" placeholder="Password" name="password"/>
-                 </div>
+   return (
+      <div className="contact_section layout_padding">
+         <div className="container">
+            <div className="contact_main">
+               <h1 className="request_text">Login</h1>
+               <form action="/action_page.php">
+                  <div className="form-group">
+                     <input type="text" className="email-bt" placeholder="Email" name="email" />
+                  </div>
+                  <div className="form-group">
+                     <input type="text" className="email-bt" placeholder="Password" name="password" />
+                  </div>
 
-              </form>
-              <div className="send_btn"><a href="/send">Login</a></div>
-           </div>
-        </div>
-     </div>
-    )
+               </form>
+               <div className="container_send">
+
+                  <input className="send_btn" type="submit" value="Login" />
+               </div>
+            </div>
+         </div>
+      </div>
+   );
 }
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,28 +1,31 @@
 import React from "react";
 
 export default function Register() {
-    return(
-        <div className="contact_section layout_padding">
-        <div className="container">
-           <div className="contact_main">
-              <h1 className="request_text">Register</h1>
-              <form action="/action_page.php">
-                 <div className="form-group">
-                    <input type="text" className="email-bt" placeholder="Username" name="username"/>
-                 </div>
-                 <div className="form-group">
-                    <input type="text" className="email-bt" placeholder="Email" name="email"/>
-                 </div>
-                 <div className="form-group">
-                    <input type="text" className="email-bt" placeholder="Password" name="password"/>
-                 </div>
-                 <div className="form-group">
-                    <input type="text" className="email-bt" placeholder="Repeat Password" name="repass"/>
-                 </div>
-              </form>
-              <div className="send_btn"><a href="/send">Register</a></div>
-           </div>
-        </div>
-     </div>
-    )
+   return (
+      <div className="contact_section layout_padding">
+         <div className="container">
+            <div className="contact_main">
+               <h1 className="request_text">Register</h1>
+               <form action="/action_page.php">
+                  <div className="form-group">
+                     <input type="text" className="email-bt" placeholder="Username" name="username" />
+                  </div>
+                  <div className="form-group">
+                     <input type="text" className="email-bt" placeholder="Email" name="email" />
+                  </div>
+                  <div className="form-group">
+                     <input type="text" className="email-bt" placeholder="Password" name="password" />
+                  </div>
+                  <div className="form-group">
+                     <input type="text" className="email-bt" placeholder="Repeat Password" name="repass" />
+                  </div>
+               </form>
+               <div className="container_send">
+
+                  <input className="send_btn" type="submit" value="Register" />
+               </div>
+            </div>
+         </div>
+      </div>
+   );
 }
