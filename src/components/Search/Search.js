60f0a001cc33412f7ac93b06dd2e53bc6ff38427
silviqkamenova1 <117118888+silviqkamenova1@users.noreply.github.com>
2023-12-05 17:02:58 +0200

    add

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,36 +1,33 @@
-
+import './Search.css'
 
 
 export default function Search() {
     return (
-        <form className="search-form">
-            <h2>
-                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="user" className="svg-inline--fa fa-user SearchBar_icon__cXpTg" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                    <path fill="currentColor" d="M224 256c70.7 0 128-57.31 128-128s-57.3-128-128-128C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3C77.61 304 0 381.6 0 477.3c0 19.14 15.52 34.67 34.66 34.67h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304z">
-                    </path>
-                </svg>
-                <span>Users</span>
-            </h2>
-            <div className="search-input-container">
-                <input type="text" placeholder="Please, select the search criteria" name="search" />
-                <button className="btn close-btn">
-                    <i className="fa-solid fa-xmark" />
-                </button>
-                <button className="btn" title="Please, select the search criteria">
-                    <i className="fa-solid fa-magnifying-glass" />
-                </button>
+        <section id="search" className='border'>
+            <div className="form">
+                <h2>Search</h2>
+                <form className="search-form">
+                    <input type="text" name="search" id="search-input" />
+                    <button className="button-list">Search</button>
+                </form>
             </div>
-            <div className="filter">
-                <span>Search Criteria:</span>
-                <select name="criteria" className="criteria">
-                    <option value>Not selected</option>
-                    <option value>First Name</option>
-                    <option value>Last Name</option>
-                    <option value>Email</option>
-                    <option value>Phone</option>
-                </select>
-            </div>
-        </form>
+            <h4>Results:</h4>
+            <div className="search-result">
+                <div class="fruit">
+                    <img src="https://www.shutterstock.com/image-photo/yellow-black-29er-mountainbike-thick-600nw-1498702814.jpg" alt="example1" />
+                    <h3 class="title">Name</h3>
+                    <p class="description">$Price</p>
+                    <a class="details-btn" href="">More Info</a>
+                </div>
+                <div class="fruit">
+                    <img src="https://www.shutterstock.com/image-photo/yellow-black-29er-mountainbike-thick-600nw-1498702814.jpg" alt="example1" />
+                    <h3 class="title">Name</h3>
+                    <p class="description">$Price</p>
+                    <a class="details-btn" href="">More Info</a>
+                </div>
+                <p className="no-result">No result.</p>
 
+            </div>
+        </section>
     );
 };
