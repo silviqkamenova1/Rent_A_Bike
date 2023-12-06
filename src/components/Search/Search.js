509896b14622a79cc7489d65500caa1783e66f88
silviqkamenova1 <117118888+silviqkamenova1@users.noreply.github.com>
2023-12-06 14:11:36 +0200

    search functionality

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,24 +1,58 @@
+import { useEffect, useState } from 'react';
 import './Search.css';
 import SearchElement from './SearchElement/SearchElement';
 
-export default function Search({
-    bikes
-}) {
- 
+export default function Search() {
+    const [data, setData] = useState([]);
+
+    const fetchData = async () => {
+  
+      return fetch("http://localhost:3030/data/bikes")
+  
+        .then((res) => res.json())
+  
+        .then((d) => setData(d));
+  
+    };
+  
+    useEffect(() => {
+  
+      fetchData();
+  
+    }, []);
+  
+    const [query, setQuery] = useState("");
+  
+    const search_parameters = Object.keys(Object.assign({}, ...data));
+    
+    function search(data) {
+  
+      const result = data.filter((data) =>
+  
+        search_parameters.some((parameter) =>
+        //console.log(typeof String(data[parameter]))
+        String(data[parameter]).toLowerCase().includes(query)
+  
+        )
+  
+      );
+      
+      return result;
+    }
     return (
         <section id="search" className='border '>
                 <div className="form">
                     <h2>Search</h2>
                     <form className="search-form">
-                        <input type="text" name="search" id="search-input" />
-                        <button className="button-list">Search</button>
+                        <input type="search" name="search" id="search-input" onChange={(e) => setQuery(e.target.value)}/>
+                        {/* <button className="button-list">Search</button> */}
                     </form>
                 </div>
                 <h4>Results:</h4>
                 
 
                 <div className="search-result">
-                {bikes.map(x => <SearchElement key={x._id} {...x}/>)}
+                {search(data).map(x => <SearchElement key={x._id} {...x}/>)}
                     {/* <div class="fruit">
                         <img src="https://www.shutterstock.com/image-photo/yellow-black-29er-mountainbike-thick-600nw-1498702814.jpg" alt="example1" />
                         <h3 class="title">Cube</h3>
@@ -36,7 +70,7 @@ export default function Search({
 
                 </div>
               
-                {bikes.length == 0 && (<p className="no-result">No result.</p>)}
+                {data.length == 0 && (<p className="no-result">No result.</p>)}
             
         </section>
     );
--- src/components/Search/SearchElement/SearchElement.js
+++ src/components/Search/SearchElement/SearchElement.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function SearchElement({
     _id,
-    model,
+    frame,
     type,
     price,
     image,
@@ -12,8 +12,8 @@ export default function SearchElement({
 
     return (
         <div className="fruit">
-            <img src="https://www.shutterstock.com/image-photo/yellow-black-29er-mountainbike-thick-600nw-1498702814.jpg" alt="example1" />
-            <h3 className="title">{model}</h3>
+            <img src={image} alt="example1" />
+            <h3 className="title">{frame}</h3>
             <p className="description"><strong>{type}</strong></p>
             <p className="description"><strong>$ {price}</strong></p>
             <Link className="details-btn" to={`/catalog/${_id}`}>More Info</Link>
